b8399f3124c820e770a408494bb981ebdec3787c
vovkahimik <vovkahimik@gmail.com>
2020-09-14 22:53:15 +0300

    Todolist - TS - 02 - props 1-4

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,26 @@ import {Todolist} from "./Todolist";
 
 function App() {
 
-    const tasks1 = [
+    let tasks1 = [
         {id: 1, title: 'HTML&CSS', isDone: true},
         {id: 2, title: 'JS', isDone: true},
-        {id: 3, title: 'ReactJS', isDone: true}
+        {id: 3, title: 'ReactJS', isDone: false},
+        {id: 4, title: 'Rest API', isDone: true},
+        {id: 5, title: 'GraphQL', isDone: true}
     ]
 
-    const tasks2 = [
-        {id: 1, title: 'Song1', isDone: true},
-        {id: 2, title: 'Song2', isDone: true},
-        {id: 3, title: 'Song3', isDone: true}
-    ]
+    function removeTask(id: number) {
+        tasks1 = tasks1.filter(task => task.id !== id)
+        console.log(tasks1)
+    }
 
     return (
         <div className="App">
-            <Todolist title='What to learn' tasks={tasks1}/>
-            <Todolist title='Songs' tasks={tasks2}/>
+            <Todolist
+                title='What to learn'
+                tasks={tasks1}
+                removeTask={removeTask}
+            />
         </div>
     )
 }
