896e6a485426cb5cbf03af22f22daf1b3b5bed7e
vovkahimik <vovkahimik@gmail.com>
2020-09-28 20:37:58 +0300

    Todolist - TS - 06 - AddItemForm, EditableSpan 9-10 - add function activateViewMode

--- src/EditedSpan.tsx
+++ src/EditedSpan.tsx
@@ -8,9 +8,12 @@ export function EditedSpan(props: ValueType) {
 
     let [editMode, setEditMode] = useState(false)
     const activateEditMode = () => setEditMode(true)
+    const activateViewMode = () => setEditMode(false)
 
     return editMode
-        ? <input value={props.value}/>
+        ? <input
+            value={props.value}
+            autoFocus onBlur={activateViewMode}/>
         : <span onDoubleClick={activateEditMode}>{props.value}</span>
 
 }
