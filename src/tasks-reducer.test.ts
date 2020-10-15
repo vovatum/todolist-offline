8249e8dc2362241cdcdaed33a778b59c8401edfb
vovkahimik <vovkahimik@gmail.com>
2020-10-15 19:50:00 +0300

    09 - reducers + tests + tdd part 2 8 todolists-tasks-reducer.test.ts

--- src/tasks-reducer.test.ts
+++ src/tasks-reducer.test.ts
@@ -118,19 +118,4 @@ test('new array should be added when new todolist is added', () => {
     expect(keys.length).toBe(3);
     expect(endState[newKey]).toEqual([]);
 });
-test('ids should be equals', () => {
-    const startTasksState: TaskStateType = {};
-    const startTodolistsState: Array<TodolistType> = [];
 
-    const action = AddTodolistAC("new todolist");
-
-    const endTasksState = tasksReducer(startTasksState, action)
-    const endTodolistsState = todolistsReducer(startTodolistsState, action)
-
-    const keys = Object.keys(endTasksState);
-    const idFromTasks = keys[0];
-    const idFromTodolists = endTodolistsState[0].id;
-
-    expect(idFromTasks).toBe(action.todolistId);
-    expect(idFromTodolists).toBe(action.todolistId);
-});
