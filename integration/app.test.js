02d6e9919b9bd08e83fb2beda8366b6bfcc42fe9
vovkahimik <vovkahimik@gmail.com>
2020-11-24 21:26:11 +0300

    TodoList - TS - 12- storybook, snapshot testing 12 create snapshot-integration tests for other components

--- integration/app.test.js
+++ integration/app.test.js
@@ -8,3 +8,27 @@ describe('addItemForm', () => {
         expect(image).toMatchImageSnapshot();
     });
 });
+
+describe('AppWithRedux', () => {
+    it('base example, visually looks correct', async () => {
+        await page.goto('http://localhost:6006/iframe.html?id=appwithredux-stories--app-with-redux-base-example');
+        const image = await page.screenshot();
+        expect(image).toMatchImageSnapshot();
+    });
+});
+
+describe('editablespan', () => {
+    it('base example, visually looks correct', async () => {
+        await page.goto('http://localhost:6006/iframe.html?id=editablespan-stories--editable-span-form-base-example');
+        const image = await page.screenshot();
+        expect(image).toMatchImageSnapshot();
+    });
+});
+
+describe('task', () => {
+    it('base example, visually looks correct', async () => {
+        await page.goto('http://localhost:6006/iframe.html?id=task-stories--task-base-example');
+        const image = await page.screenshot();
+        expect(image).toMatchImageSnapshot();
+    });
+});
